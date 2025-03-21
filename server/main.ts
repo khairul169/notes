ae548924ae88f3a39f6b87aeb57b8dcb4dadc1bf
Khairul Hidayat <khai@rul.sh>
2025-03-21 07:03:40 +0700

    feat: serve frontend static files on prod

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,19 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import api from "./api";
 import { initDb } from "./lib/db";
+import path from "path";
 
 const app = new Hono()
   //
   .route("/api", api);
 
 initDb();
+
+if (import.meta.env.NODE_ENV === "production") {
+  // serve frontend
+  app.use("*", serveStatic({ root: path.resolve(process.cwd(), "dist") }));
+}
 
 export type APIType = typeof api;
 export default app;
